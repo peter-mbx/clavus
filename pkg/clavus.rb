43ff6f3ec9572f8cdec4014af5beb08085c23dc6
Pietro Mobrici <pietro.mobrici@gmail.com>
2024-04-12 16:03:33 +0200

    pkg: fix homebrew Formula

--- pkg/clavus.rb
+++ pkg/clavus.rb
@@ -1,14 +1,19 @@
 class Clavus < Formula
-    desc "In vetusta bibliotheca, clavum lateris aperiebat fores librarii occultum, ubi sapientia et arcana conservabantur."
-    homepage "https://github.com/peter-mbx/clavus"
-    url "https://github.com/peter-mbx/clavus/archive/0.0.1.tar.gz"
-    sha256 "bb64672b75d2d459063820b65364798ab9569e14e7ee04a1d5ce514c3c5c18b4"
-    license "MIT"
-  
-    depends_on "rust" => :build
-  
-    def install
-      system "cargo", "build", "--release"
-      bin.install "target/release/clavus"
-    end
+  desc "Clavum Lateris"
+  homepage "https://github.com/peter-mbx/clavus"
+
+  url "https://github.com/peter-mbx/clavus.git",
+      tag:      "0.0.1",
+      revision: "3d7e77f3cab1118b7e30c88a3ebb55e9c966a26e"
+
+  license "MIT"
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
+
+  test do
+    system "cargo", "test"
+  end
+end
